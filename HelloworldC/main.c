265ede8659149b78bafd826dc164bc019938a94a
wosunny <wosunny@126.com>
2015-04-28 12:50:53 +0800

    Initial Commit

---
+++ HelloworldC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloworldC
+//
+//  Created by Wosunny on 15/4/28.
+//  Copyright (c) 2015年 Wosunny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
